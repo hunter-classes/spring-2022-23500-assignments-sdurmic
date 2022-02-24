2f291b3343f5451c4cb5a5a6642d9404e7a83cf3
Sara Durmic <saradurmic@Saras-Air.fios-router.home>
2022-02-24 02:10:00 -0500

    added node

--- assignments/linked_list/Node.cpp
+++ assignments/linked_list/Node.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include "Node.h"
 
+
+Node::Node() : next(nullptr)
+{
+
+}
+
+Node::Node(string data)
+{
+    this->data = data;
+    this->next = nullptr;
+}
+
+Node::Node(string data, Node *next)
+{
+    this->data = data;
+    this->next = next;
+}
+
+void Node::setData(string data)
+{
+    this->data = data;
+}
+
+void Node::setNext(Node *next)
+{
+    this->next = next;
+}
+
+string Node::getData()
+{
+    return this->data;
+}
+
+Node *Node::getNext()
+{
+    return this->next;
+}

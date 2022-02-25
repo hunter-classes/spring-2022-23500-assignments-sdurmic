896c5fac99ce4f7240225c236118d76a2151d0e2
Sara Durmic <saradurmic@Saras-Air.fios-router.home>
2022-02-24 23:49:42 -0500

    done

---
+++ assignments/recursive-search/Knight.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int knightMove(int x, int y, int turn, int pos[5][5], int xTurn[8], int yTurn[8]);
+void board(int n, int x1, int y1);
--- assignments/recursive-search/main.cpp
+++ assignments/recursive-search/main.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+#include "Knight.h"
+using namespace std;
 
+int main()
+{	
+    int n = 5;
+	board(n, 3, 5);	
+	return 0;
+}

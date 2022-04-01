e627c8bd90b755212d5cc2ae62e814cce87faa2d
Sara Durmic <saradurmic@Saras-Air.fios-router.home>
2022-04-01 03:26:11 -0400

    part 1

--- assignments/pointer-maze/Labyrinth.cpp
+++ assignments/pointer-maze/Labyrinth.cpp
@@ -4,59 +4,41 @@ using namespace std;
 
 bool isPathToFreedom(MazeCell* start, const string& moves)
 {
-    int magItem[3]={0, 0, 0};
-    Item item=start->whatsHere;
+	bool wand = false;
+    bool spellbook = false;
+	bool potion = false;
 
-    if(item == Item::SPELLBOOK) magItem[1] += 1;
-    if(item == Item::POTION) magItem[0] += 1;
-    if(item == Item::WAND) magItem[2] += 1;
-   
-    for(int x=0; x<moves.length(); x++)
+	for(int x=0; x<moves.length(); x++)
     {
-   	    char move = moves[x];
-
-   	    if (move=='N') 
-            start=start->north;
-
-   	    else if(move=='W') 
-            start=start->west;
-
-        else if(move=='S') 
-            start=start->south;
-
-   	    else if(move=='E') 
-            start=start->east;
-
-		if (start==nullptr)
-            return false;
-		else
+		if(moves[x]=='N')
         {
-			item=start->whatsHere;
-
-			if(item==Item::SPELLBOOK)
-            {	
-				magItem[1]+=1;
-				cout << "Spellbook discovered" << endl;
-			}
-
-			if(item==Item::POTION)
-            {
-				magItem[0]+=1;
-				cout << "Potion discovered" << endl;
-			}
-
-			if(item==Item::WAND)
-            {
-				magItem[2] += 1;
-				cout << "Wand discovered" << endl;
-			}
+			start = start->north;
+		}else if(moves[x]=='S')
+        {
+            start = start->south;
+		}else if(moves[x]=='W')
+        {
+            start = start->west;
+		}else if(moves[x] == 'E')
+        {
+            start = start->east;
 		}
-    }
-   
-    for (int x=0; x<sizeof(magItem)/sizeof(magItem[0]); x++)
+
+		if(start->whatsHere == Item::SPELLBOOK)
+        {
+			spellbook = true;
+		}else if(start->whatsHere == Item::POTION)
+        {
+            potion = true;	
+		}else if(start->whatsHere == Item::WAND)
+        {
+            wand = true;
+		}
+	}
+	if(spellbook && potion && wand)
     {
-   	    if(magItem[x]<=0)
-            return false;
-    }
-	return true;
+		return true;
+	}else{
+		return false;
+	}
 }

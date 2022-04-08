f9d6f29fcbb68678d8764fab1b22140ff182cf8d
sdurmic <70329596+sdurmic@users.noreply.github.com>
2022-04-08 03:33:35 -0400

    Update sorts.cpp

--- assignments/sorts/sorts.cpp
+++ assignments/sorts/sorts.cpp
@@ -34,13 +34,10 @@ vector<int> ssort(vector<int> a)
 {
     int i,min_index,j,tmp;
     int len = a.size();
-    // loop through the vector from 0 to end
     for (i=0;i<len; i++)
     {
-        // find the smallest value from i to the end
         min_index = find_min_index(a,i,len);
     
-        // swap it into position i
         tmp = a[i];
         a[i] = a[min_index];
         a[min_index] = tmp;
@@ -48,17 +45,10 @@ vector<int> ssort(vector<int> a)
     return a;
 }
 
-/*
-  left and right are already sorted
-  merged should be a new vector with all the
-  elements in left and right, and result
-  should be sorted
-*/
 vector<int> merge(vector<int> left,vector<int> right)
 {
     vector<int> merged;
 
-    // your code here
     int l = 0;
     int r = 0;
 
@@ -73,13 +63,11 @@ vector<int> merge(vector<int> left,vector<int> right)
             r++;
         }
     }
-    // add any extra elements in left
     while (l < left.size())
     {
         merged.push_back(left[l]);
         l++;
     }
-    // add any extra elements in right
     while (r < right.size())
     {
         merged.push_back(right[r]);
@@ -108,31 +96,195 @@ vector<int> msort(vector<int> v)
     return merge(left,right);
 }
 
-int main()
+vector<int> qsort(vector<int> list)
 {
-    int size=20;
-    int max_val=100;
+    int i,j;
+  
+    if(list.size() <= 1)
+    {
+        return list;
+    }
+
+    int pivot = list[0];
+  
+    vector<int> lower,higher;
+
+    for(i=1; i < list.size(); i++)
+    {
+        if (list[i] < pivot)
+        {
+            lower.push_back(list[i]);
+        }else{
+            higher.push_back(list[i]);
+        }
+    }
+  
+    lower = qsort(lower);
+    higher = qsort(higher);
+
+    for(i=0 ; i < lower.size(); i++)
+    {
+        list[i]=lower[i];
+    }
+
+    list[i] = pivot;
+    i++;
+
+    for(j=0;j<higher.size();j++)
+    {
+        list[i] = higher[j];
+        i++;
+    }
+  
+    return list; 
+}
+
+void swap(vector<int>& list, int i, int j)
+{
+    int temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
+}
+
+// grabbed code from pandaericbear
+void qsort2(vector<int>& list, int low, int high)
+{
+    int p = list[low];
+    int goodPivot;
+    int first = list[low];
+    int mid = list[low + (high-low) / 2];
+    int last = list[high];
+    if(first>mid)
+    {
+        if(first<last)
+            goodPivot = first;
+        else if(mid>last)
+            goodPivot = mid;
+        else
+            goodPivot = last;
+    }else{
+        if(last > mid)
+            goodPivot = mid;
+        else if (first > last)
+            goodPivot = first;
+        else
+            goodPivot = last;
+    }
+    int l = low;
+    int h = high;
+
+    while(l <= h)
+    {
+        while(list[l] < goodPivot)
+        {
+            l++;
+        }
+        while(list[h] > goodPivot)
+        {
+            h--;
+        }
+        if(l <= h)
+        {
+            swap(list, l, h);
+            l++;
+            h--;
+        }
+    }
+    if(low < high)
+    {
+        qsort2(list, low, l-1);
+        qsort2(list, l, high);
+    }
+}
+
+
+void print_help(char *command_name)
+{
+    cout << "Usage: "<< command_name;
+    cout << " [-h|-p|-m N|-s N|-a algorithm]\n\n";
+    cout << " -m MAX_ELEMENT_SIZE\n";
+    cout << " -s DATA_SET_SIZE\n";
+    cout << " -a[s|m]: s - selection, m - merge\n";
+}
+
+extern char *optarg;
+
+int main(int argc, char *argv[])
+{
+    int size = 25000;
+    int max_val = 200; 
+
+    char algorithm = 's' ; 
+    bool print = false; 
+    char c;
+
+    while((c = getopt(argc, argv, "phs:m:a:")) != -1)
+    {
+        switch (c) 
+        {
+            case 'h':
+	            print_help(argv[0]);
+	            exit(0);
+	            break;
+            case 'p':
+	            print = true;
+	            break;
+            case 's':
+	            size = stoi(optarg);
+	            break;
+            case 'm':
+	            max_val = stoi(optarg);
+	            break;
+            case 'a':
+	            algorithm = optarg[0];
+        }
+    }
 
     srand(time(nullptr));
     vector<int> a(size);
     int i;
-    for (i=0;i<size; i++)
+    for(i=0;i<size; i++)
     {
         a[i] = rand()%max_val;
     }
-    /*
-    print_vector(a);
-    std::cout << "\n";
-    a = ssort(a);
-    print_vector(a);
-    */
-    vector<int> left = {1,2,5,6,10,15};
-    vector<int> right = {3,7,8,12,16,19,20};
-    print_vector(left);
-    print_vector(right);
-    vector<int> m = merge(left,right);
-    print_vector(m);
-    vector<int> ms = {80,9,7,333,1,4,1800,10,11,9,22,33};
-    print_vector(msort(ms));
+
+    if(print)
+    {
+        print_vector(a);
+        cout << "\n";
+    }
+
+    cout << "Starting the sort!\n";
+    struct timeval tp;
+
+    gettimeofday(&tp,NULL);
+    long start_time = tp.tv_sec *1000 + tp.tv_usec / 1000;
+
+    if (algorithm == 's')
+    {
+        a = ssort(a);
+    }else if(algorithm == 'm')
+    {
+        a = msort(a);
+    }else if(algorithm == 'q') 
+    {
+        a = qsort(a);
+    }else if(algorithm == 'Q')
+    {
+        qsort2(a, 0, size);
+    }
+
+    gettimeofday(&tp,NULL);
+    long current_time = tp.tv_sec *1000 + tp.tv_usec / 1000;
+
+    long elapsed = current_time - start_time;
+
+    if(print) 
+    {
+        print_vector(a);
+    }
+    cout << "Algorithm: " << algorithm << "\n";
+    cout << "Time: " << elapsed << "\n";
+
     return 0;
 }

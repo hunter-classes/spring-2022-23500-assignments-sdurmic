1b83e269e715ccc4b217db305559fb7b232b5180
sdurmic <70329596+sdurmic@users.noreply.github.com>
2022-04-27 15:23:42 -0400

    Task 2

--- assignments/BSTree/main.cpp
+++ assignments/BSTree/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include "Node.h"
+#include "BSTree.h"
+using namespace std;
+
+int main()
+{
+    Node *root = new Node(20);
+    Node *n1 = new Node(30);
+    root->setLeft(n1);
+    Node *n2 = new Node(40);
+    root->setRight(n2);
+    Node *n3 = new Node(50);
+    root->getLeft()->setLeft(n3);
+
+    cout << root->getData() << endl;
+    cout << n1->getData() << endl;
+    cout << n2->getData() << endl;
+    cout << n3->getData() << endl;
+
+    return 0;
+}
 
